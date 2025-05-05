47cd73d76c00520454db88a2a874ff26312e2916
VittalT <vittalt@mit.edu>
2025-05-04 23:14:49 -0400

    43

--- homegrid/DQN.py
+++ homegrid/DQN.py
@@ -165,7 +165,7 @@ class DQNAgent:
         self.epsilon = 1.0
         self.batch_size = 32  # Larger batch size for better gradient estimates
         self.episodes = episodes
-        self.epsilon_decay = 0.999  # Slower decay helps explore more thoroughly
+        self.epsilon_decay = 0.995  # Slower decay helps explore more thoroughly
         self.epsilon_min = 0.05  # Higher minimum exploration rate
         self.llm_cost = 0.01
         self.num_llm_calls = 0
